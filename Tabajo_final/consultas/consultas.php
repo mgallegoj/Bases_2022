01cb9a8c6ed3e7a9166604ff00433b0869f11602
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-06-22 22:35:28 -0500

    Consultas terminadas

--- Tabajo_final/consultas/consultas.php
+++ Tabajo_final/consultas/consultas.php
@@ -12,7 +12,7 @@
                                 <input class="form-check-input" type="radio" name="consulta" id="consulta1" value="1" checked hidden>
                             </div>
                             <div class="form-group d-grid gap-2">
-                                <input type="submit" class="btn btn-primary" value="Ganadores">
+                                <input type="submit" class="btn btn-primary" value="Consulta 1">
                             </div>
                         </form>
                         <form action="consultas.php" method="get">
@@ -20,7 +20,7 @@
                                 <input class="form-check-input" type="radio" name="consulta" id="consulta2" value="2" checked hidden>
                             </div>
                             <div class="form-group d-grid gap-2">
-                                <input type="submit" class="btn btn-primary" value="Información usuarios">
+                                <input type="submit" class="btn btn-primary" value="Consulta 2">
                             </div>
                         </form>
                     </div>
@@ -32,62 +32,65 @@
                         require('../configuraciones/conexion.php');
                         if ($_GET['consulta'] == '1') {
                             $query="SELECT numero_de_cedula, nombre FROM fiador WHERE EXISTS(SELECT fiador AS fiador_emp FROM empeno WHERE empeno.fiador = numero_de_cedula) AND ((SELECT SUM(valor) FROM empeno WHERE empeno.fiador = numero_de_cedula) > 1000) AND ((SELECT COUNT(fiador) FROM contrato WHERE contrato.fiador = numero_de_cedula) >= 3) AND (SELECT COUNT(empeno) FROM contrato WHERE contrato.empeno = (SELECT codigo AS codigo_emp FROM empeno WHERE empeno.fiador = numero_de_cedula)) = 0";
-                            $consulta1 = mysqli_query($conn, $query) or die(mysqli_error($conn));
+                            $consulta1 = mysqli_query($conn, $query) or die(mysqli_error($conn));?>
                             <table class="table border-rounded table-bordered table-hover">
-                                <thead>
+                            <thead>
                                     <tr class="table-dark">
                                         <th scope="col">Número de documento</th>
                                         <th scope="col">Nombre</th>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                if($consulta1){
-                                    foreach($consulta1 as $fila){
-                                        ?>
-                                <tr>
-                                    <td>
-                                    <?=$fila['codigo'];?>
-                                    </td>
-                                    <td>
-                                    <?=$fila['valor'];?>
-                                    </td>
-                                    <?php
-                                    
-                                }
-                            }
-                                </tbody>
-                            </table>
+                            </thead>
+                            <tbody>
+                            <?php
+                                    if($consulta1){
+                                        foreach($consulta1 as $fila){
+                                    ?>
+                                        <tr>
+                                            <td>
+                                                <?=$fila['numero_de_cedula'];?>
+                                            </td>
+                                            <td>
+                                                <?=$fila['nombre'];?>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                        
+                                        }
+                                    }
+                                
+                            
                     } else{
                             $query="SELECT codigo, valor FROM contrato WHERE valor > (SELECT valor AS valor_emp FROM empeno WHERE contrato.empeno = empeno.codigo) AND (contrato.fiador = (SELECT fiador AS fiador_emp FROM empeno WHERE contrato.empeno = empeno.codigo))";
                             $consulta2 = mysqli_query($conn, $query) or die(mysqli_error($conn));?>
                             <table class="table border-rounded table-bordered table-hover">
-                            <thead>
-                                <tr class="table-dark">
-                                    <th scope="col">Código del contrato</th>
-                                    <th scope="col">Valor del contrato</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                            if($consulta2){
-                                foreach($consulta2 as $fila){
-                                    ?>
-                        <tr>
-                            <td>
-                            <?=$fila['codigo'];?>
-                            </td>
-                            <td>
-                            <?=$fila['valor'];?>
-                            </td>
+                                <thead>
+                                    <tr class="table-dark">
+                                        <th scope="col">Código del contrato</th>
+                                        <th scope="col">Valor del contrato</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
                                     <?php
-                                }
+                                    if($consulta2){
+                                        foreach($consulta2 as $fila){
+                                    ?>
+                                        <tr>
+                                            <td>
+                                                <?=$fila['codigo'];?>
+                                            </td>
+                                            <td>
+                                                <?=$fila['valor'];?>
+                                            </td>
+                                        </tr>
+                                            <?php
+                                        
+                                        }
+                                    }
+                                
                             }
-                        
-                    }
-                }?>
-                    </tbody>
-                            </table>
+                        }?>
+                                </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
